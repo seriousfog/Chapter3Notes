cdb038da8f293dd8a09cb1418878000a0ccca40d
t9uo <t9uo@fresnou.org>
2023-03-14 10:16:22 -0700

    initial commit

--- functions.js
+++ functions.js
@@ -38,8 +38,8 @@
 // printMax(2, 75);
 // printMax(2, 2);
 
-function sum(num1, num2){
-    console.log(`${num1}+${num2}=${num1+num2}`);
-}
-sum(7, 10);
-sum(27, 1);
+// function sum(num1, num2){
+//     console.log(`${num1}+${num2}=${num1+num2}`);
+// }
+// sum(7, 10);
+// sum(27, 1);
