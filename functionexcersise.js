5b7e96115f8c2858a0926868424c61bc34f3af9f
t9uo <t9uo@fresnou.org>
2023-03-14 10:29:15 -0700

    initial commit

---
+++ functionexcersise.js
@@ -0,0 +1,46 @@
+//Find the area of a rectangle
+
+// function product(num1, num2){
+//     console.log(`${num1}*${num2}=${num1*num2}`);
+// }
+// product(10, 2);
+// product(10, 2);
+
+
+//Find the area of a circle
+
+// function product(radius, pi){
+//     console.log(`${radius} * ${pi} = ${radius*pi}`);
+// }
+// product(6, 3.14);
+// product(6, 3.14);
+
+
+//findMin
+
+// function printMax(x, y){
+// if (x<y){
+// console.log(`the smaller number is ${x}`);
+// } else {
+// console.log(`the smaller number is ${y}`);
+// }
+// }
+// printMax (16, 9);
+// printMax(3, 75);
+// printMax(2, 5);
+
+
+//checkEquality
+
+function printMax(x, y){
+if (x==y){
+console.log(`${x} and ${y} are equal`);
+} else {
+console.log(`${x} and ${y} are not equal`);
+}
+}
+printMax (16, 9);
+printMax(75, 75);
+printMax(4.9, 5);
+
+
