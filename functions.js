0b42f5017cded579367c835de65757e20eaf3c5b
t9uo <t9uo@fresnou.org>
2023-03-14 10:00:35 -0700

    initial commit

---
+++ functions.js
@@ -0,0 +1,45 @@
+//Function Notes
+//Step 1: Define the function
+
+// function sayHello(){
+// console.log('Hello');
+// }
+
+//2. call or use the function
+
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+//define
+
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if (x>y){
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+//
+// printMax();
+
+// function printMax(x, y){
+// if (x>y){
+// console.log(`the larger number is ${x}`);
+// } else {
+// console.log(`the larger number is ${y}`);
+// }
+// }
+// printMax (16, 9);
+// printMax(2, 75);
+// printMax(2, 2);
+
+function sum(num1, num2){
+    console.log(`${num1}+${num2}=${num1+num2}`);
+}
+sum(7, 10);
+sum(27, 1);
