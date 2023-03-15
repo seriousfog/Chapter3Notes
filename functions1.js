079cc33d32f2e23accb6d79699193753f29b9724
t9uo <t9uo@fresnou.org>
2023-03-15 10:30:07 -0700

    initial commit

---
+++ functions1.js
@@ -0,0 +1,99 @@
+//Find the area of a rectangle
+
+// function product(num1, num2){
+//     console.log(`${num1}*${num2}=${num1*num2}`);
+// }
+// product(10, 2);
+// product(10, 2);
+
+
+//Find the area of a circle
+
+// function product(radius, pi){
+//     console.log(`${pi} * ${radius} * ${radius} = ${radius*pi}`);
+// }
+// product(6, 3.14);
+// product(6, 3.14);
+
+
+//findMin
+
+// function printMax(x, y){
+// if (x<y){
+// console.log(`the smaller number is ${x}`);
+// } else {
+// console.log(`the smaller number is ${y}`);
+// }
+// }
+// printMax (16, 9);
+// printMax(3, 75);
+// printMax(2, 5);
+
+
+//checkEquality
+
+// function printMax(x, y){
+// if (x==y){
+// console.log(`${x} and ${y} are equal`);
+// } else {
+// console.log(`${x} and ${y} are not equal`);
+// }
+// }
+// printMax (16, 9);
+// printMax(75, 75);
+// printMax(4.9, 5);
+
+//calculateLargestRectangle
+
+// function product(rec1, rec2){
+//     console.log(`${rec1}*${rec2}=${rec1*rec2}`);
+// }
+// product(10, 2);
+// product(3, 11);
+// function printMax(rec1, rec2){
+// if (rec1>rec2){
+// console.log(`the bigger rectangle is ${rec1}`);
+// } else {
+// console.log(`the bigger rectangle is ${rec2}`);
+// }
+// }
+// printMax (10, 2);
+// printMax(3, 11);
+
+
+//charityCollection
+
+// function sum(num1, num2, num3, num4){
+//     console.log(`${num1} + ${num2} + ${num3} = ${num4}`);
+// }
+// sum(500, 500, 500 ,1500)
+// let num4 = 1500
+// {
+//     if( num4 > 1000 )
+//         console.log(`LETS DOUBLE IT GIVE IT TO YOU, YOU GAIN ${num4*2} DOLLARS`)
+//     else{
+//         console.log(num4)
+//     }
+// }
+
+
+//stateOfMatter
+function number(temp){
+    console.log(`${temp} FERENHEIGHT`);
+}
+number(74)
+{
+    let temp = 74;
+    if( temp > 32 ){
+    console.log('SOLID')
+    }
+    else if ( temp > 212 ){
+        console.log('GAS')
+    }
+    else if ( temp<100 ){
+        console.log('SOLID')
+    }
+}
+
+
+
