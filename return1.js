61017d418886adf0079c8ce4cff435d41fa56563
t9uo <t9uo@fresnou.org>
2023-03-17 10:28:53 -0700

    initial commit

---
+++ return1.js
@@ -0,0 +1,57 @@
+const readLine = require ('readline-sync');
+
+
+// function nameswap(){
+//     let first = readLine.question("Enter your first name: ");
+//     let second = readLine.question("Enter your last name: ");
+//     return (`${second}, ${first}`)
+// }
+// console.log(nameswap())
+
+
+// function numbergenerator(){
+//     let interger = readLine.question("Enter a number 0-9: ");
+//     let interger2 = readLine.question("Enter another number 0-9: ");
+//     return (`${interger}${interger2}`)
+// }
+// console.log(numbergenerator())
+
+
+// function averagetemperature(){
+//     let temp1 = Number(readLine.question("Enter a temperature for Monday: "));
+//     let temp2 = Number(readLine.question("Enter a temperature for Tuesday: "));
+//     let temp3 = Number(readLine.question("Enter a temperature for Wednesday: "));
+//     let temp4 = Number(readLine.question("Enter a temperature for Thursday: "));
+//     let temp5 = Number(readLine.question("Enter a temperature for Friday: "));
+//     let temp6 = Number(readLine.question("Enter a temperature for Saturday: "));
+//     let temp7 = Number(readLine.question("Enter a temperature for Sunday: "));
+//
+//     let total = (temp1+temp2+temp3+temp4+temp5+temp6+temp7)/7
+//     return( total.toFixed(2));
+//
+// }
+// console.log(averagetemperature())
+
+
+//Charity Collection Revisited
+// function sum(num1, num2, num3, num4){
+//     console.log(`${num1} + ${num2} + ${num3} = ${num4}`);
+// }
+// sum(500, 500, 500 ,1500)
+// let num4 = 500
+// {
+//     if( num4 > 1000 && num4 < 2000 )
+//         console.log(`LETS DOUBLE IT GIVE IT TO YOU, YOU GAIN ${num4*2} DOLLARS`)
+//     else if( num4 < 1000){
+//         console.log(`Add 100 dollars to make ${num4+100}`)
+//     }
+//     else if( num4 > 2000 ){
+//         console.log(`Double it and make ${num4*2}`)
+//     }
+// }
+
+
+function dart(){
+    let round1 = Number(readLine.question("Enter a temperature for Monday: "));
+    let round2 = Number(readLine.question("Enter a temperature for Tuesday: "));
+    let round3 = Number(readLine.question("Enter a temperature for Wednesday: "));
