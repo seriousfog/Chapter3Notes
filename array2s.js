5d9adbf282ecde485b15d8b21eb0045e76cbe826
t9uo <t9uo@fresnou.org>
2023-03-28 10:27:39 -0700

    initial commit

--- array2s.js
+++ array2s.js
@@ -25,8 +25,19 @@
 
 
 //3
-function question(x){
-let numbers = ['1', '2', '3', '4', '5'];
-numbers.unshift('1', '2', '3', '4', '5')
-console.log('numbers: ' + x)
-}
+
+// function question(x){
+// let numbers = ['1', '2', '3', '4', '5'];
+// numbers.unshift('1', '2', '3', '4', '5')
+// console.log('numbers: ' + x)
+// }
+
+
+//4
+
+// function question(x){
+// let numbers = ['1', '2', '3', '4', '5'];
+// numbers.unshift('1', '2', '3', '4', '5')
+// console.log('numbers: ' + x)
+// console.log(numbers)
+// }
