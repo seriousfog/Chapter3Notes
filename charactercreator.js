e8a4669b7dd59ee69c865d31a8702214bc50be15
t9uo <t9uo@fresnou.org>
2023-04-18 10:25:13 -0700

    initial commit

---
+++ charactercreator.js
@@ -0,0 +1,60 @@
+const readLine = require ('readline-sync');
+
+let diceRolled = [];
+let character1 = {
+    //empty to start
+}
+
+createChar(character1, diceRolled);
+
+function rolld6(){
+    return Math.ceil(Math.random()*6)
+}
+
+function rollDice(amount, list){
+    for (let i=0; i<amount; i++){
+        let roll = rolld6();
+        console.log(`You rolled ${roll}!`)
+        list.push(roll);
+    }
+}
+function sum(list){
+    let sum = 0;
+    for (let i=0; i<list.length; i++){
+        sum += list[i]
+    }
+    return sum;
+}
+function rollStat(list){
+    list = []
+    rollDice(4, list);
+    return sum(list);
+}
+function createChar(character, list){
+    character.name = readLine.question("Enter Character Name: ")
+    console.log("Rolling STR")
+    character.strength = rollStat(diceRolled);
+    console.log("Rolling DEX")
+    character.dexerity = rollStat(diceRolled);
+    console.log("Rolling CON")
+    character.constitution = rollStat(diceRolled);
+    console.log("Rolling INT")
+    character.intelligence = rollStat(diceRolled);
+    console.log("Rolling WIS")
+    character.wisdom = rollStat(diceRolled);
+    console.log("Rolling CHA")
+    character.charisma = rollStat(diceRolled);
+    displayChar(character1)
+}
+
+function displayChar(char){
+    console.log('--------------------')
+    console.log(`Name: ${char.name}`);
+    console.log(`STR: ${char.strength}`);
+    console.log(`DEX: ${char.dexerity}`);
+    console.log(`CON: ${char.constitution}`);
+    console.log(`INT: ${char.intelligence}`);
+    console.log(`WIS: ${char.wisdom}`);
+    console.log(`CHA: ${char.charisma}`);
+    console.log('--------------------')
+}
