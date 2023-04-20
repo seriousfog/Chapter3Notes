f0bfa347a9bdcb1d747cd85eec000049432bf259
t9uo <t9uo@fresnou.org>
2023-04-20 10:28:12 -0700

    initial commit

--- charactercreator.js
+++ charactercreator.js
@@ -2,10 +2,17 @@ const readLine = require ('readline-sync');
 
 let diceRolled = [];
 let character1 = {
-    //empty to start
+}
+
+let character2 = {
+}
+
+let character3 = {
 }
 
 createChar(character1, diceRolled);
+createChar(character2, diceRolled);
+createChar(character3, diceRolled);
 
 function rolld6(){
     return Math.ceil(Math.random()*6)
@@ -32,6 +39,7 @@ function rollStat(list){
 }
 function createChar(character, list){
     character.name = readLine.question("Enter Character Name: ")
+    character.age = readLine.question("Enter Character Age: ")
     console.log("Rolling STR")
     character.strength = rollStat(diceRolled);
     console.log("Rolling DEX")
@@ -44,17 +52,29 @@ function createChar(character, list){
     character.wisdom = rollStat(diceRolled);
     console.log("Rolling CHA")
     character.charisma = rollStat(diceRolled);
+    console.log("Rolling Cooking Skills")
+    character.cook = rollStat(diceRolled);
+    console.log("Rolling Social Skills")
+    character.sk = rollStat(diceRolled);
+    console.log("Rolling HP")
+    character.hp = rollStat(diceRolled);
     displayChar(character1)
+    displayChar(character2)
+    displayChar(character3)
 }
 
 function displayChar(char){
-    console.log('--------------------')
+    console.log('------------------------')
     console.log(`Name: ${char.name}`);
+    console.log(`Age: ${char.age}`)
     console.log(`STR: ${char.strength}`);
     console.log(`DEX: ${char.dexerity}`);
     console.log(`CON: ${char.constitution}`);
     console.log(`INT: ${char.intelligence}`);
     console.log(`WIS: ${char.wisdom}`);
     console.log(`CHA: ${char.charisma}`);
-    console.log('--------------------')
+    console.log(`COOKING: ${char.cook}`);
+    console.log(`SOCIAL SKILLS: ${char.sk}`);
+    console.log(`HP: ${char.hp}`);
+    console.log('------------------------')
 }
